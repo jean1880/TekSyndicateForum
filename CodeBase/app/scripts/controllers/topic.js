ccddc22f4fd4b4fbc1ed1c086018950074f13c74
Jean-Luc Desroches <jean18801880@gmail.com>
2015-03-26 02:26:07 -0400

    most images fixed

--- CodeBase/app/scripts/controllers/topic.js
+++ CodeBase/app/scripts/controllers/topic.js
@@ -13,15 +13,24 @@ angular.module('tekForumApp')
             FactoryTopic.get($routeParams.id).success(function (data) {
                 $scope.topic = data;
                 $scope.postCount = data.post_stream.posts.length;
-                $scope.MAXPOSTCOUNT = data.posts_count
+                $scope.MAXPOSTCOUNT = data.posts_count;
+                $scope.FormatImages();
                 console.log($scope.topic);
+            });
+        };
+
+        $scope.FormatImages = function () {
+            $('#topic-board').find('img').each(function () {
+                $(this).addClass('img img-responsive');
+                if (!$(this).attr('src').match('^http')) {
+                    $(this).attr('src', 'https://' + $(this).attr('src'));
+                }
             });
         };
         // called when nearing bottom of the page, looks for more posts, if available
         $scope.FetchPosts = function () {
             if ($scope.postCount < $scope.MAXPOSTCOUNT) {
-                var request = '';
-                var requestAttach = '';
+                var request, requestAttach = '';
                 // build array of post ids to fetch
                 for (var i = 0; i < 20; i++) {
                     // if target id exists push into fetch array, otherwise exit loop
@@ -39,8 +48,8 @@ angular.module('tekForumApp')
 
                 // fetch posts and add to posts array
                 FactoryTopic.getPosts($scope.topic.id, request).success(function (data) {
-                    console.log(data);
                     $scope.topic.post_stream.posts.push.apply($scope.topic.post_stream.posts, data.post_stream.posts);
+                    $scope.FormatImages();
                 })
             }
         };
